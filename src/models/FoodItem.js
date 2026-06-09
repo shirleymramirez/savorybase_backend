c22dfb3f32839f391a2c366375acc74f091c9846
Shirley <shirley.mramirez@yahoo.com>
2026-06-09 09:36:00 -0700

    implemented restful api orders

---
+++ src/models/FoodItem.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const foodItemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    category: {
+      type: String,
+      enum: ["Appetizer", "Main Course", "Dessert", "Vegan", "Gluten-Free", "Seasonal", "Chef Special"],
+      required: true
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    active: {
+      type: Boolean,
+      default: true
+    },
+    stock: {
+      type: String,
+      default: "In Stock" 
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+module.exports = mongoose.model("FoodItem", foodItemSchema);
